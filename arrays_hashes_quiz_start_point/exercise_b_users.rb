c1d5e3ea6ae75dab91ded796f779b47bccbd77ce
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-11-20 16:26:05 +0000

    excercise b_steps 1-7 completed

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -1,3 +1,4 @@
+
 users = {
   "Jonathan" => {
     :twitter => "jonnyt",
@@ -55,12 +56,23 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
 # 4. Get the species of Avril's pet Monty
+p users["Avril"][:pets][0][:species]
+
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].sort.at(0)
+
 # 6. Return an array of Avril's lottery numbers that are even
+p users["Avril"][:lottery_numbers]
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers] << 7
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
